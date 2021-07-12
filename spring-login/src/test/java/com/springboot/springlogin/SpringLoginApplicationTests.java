8ef698f1a7c9ad77117e7353253b0d18c84d0c3f
hoangpham12345 <phamhuyhoang93tn@gmail.com>
2021-07-12 09:38:10 +0700

    Add initial spring boot project

---
+++ spring-login/src/test/java/com/springboot/springlogin/SpringLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.springlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
